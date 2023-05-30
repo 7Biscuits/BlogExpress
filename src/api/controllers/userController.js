2c40d4372e1fc3bcfd8b3e4e58b177ba13a3a5aa
Rudransh Srivastava <rudranshsrivastava@Rudranshs-MacBook-Air.local>
2023-05-31 01:26:00 +0530

    feat: initialized joi schema validators

--- src/api/controllers/userController.js
+++ src/api/controllers/userController.js
@@ -1,43 +1,63 @@
-const jwt = require("jwt");
+const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
 const User = require("../models/UserModel");
+const Joi = require("joi");
 
-const signup = (req, res) => {
+const signupSchema = Joi.object({
+  username: Joi.string().min(3).required(),
+  password: Joi.string().min(6).required(),
+});
+
+const loginSchema = Joi.object({
+  username: Joi.string().min(3).required(),
+  password: Joi.string().min(6).required(),
+});
+
+const signup = async (req, res) => {
   const { username, password } = req.body;
 
-  const user = new User(username, password);
+  if (await User.findOne({ username: username }))
+    return res.status(400).json({ message: "Username already exists" });
 
-  user.hash_password = bcrypt.hashSync(password, 10);
-  user.save((err, user) => {
-    if (err) return res.status({ message: err.message });
-    user.hash_password = undefined;
-    return res.json(user);
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password, salt);
+
+  const user = new User({
+    username: username,
+    password: hashedPassword,
   });
+
+  try {
+    const { error } = await signupSchema.validateAsync(req.body);
+    if (error)
+      return res.status(400).json({ message: error.details[0].message });
+    await user.save();
+    res.json({ message: "Signup successful" });
+  } catch (err) {
+    res.json({ message: err.message });
+  }
 };
 
-const signin = (req, res) => {
+const signin = async (req, res) => {
   const { username, password } = req.body;
-  User.findOne(
-    {
-      username: username,
-    },
-    (err, user) => {
-      if (err) throw err;
-      if (!user || !user.comparePassword(password))
-        return res
-          .status(res.statusCode)
-          .json({
-            message: "Authentication failed. Invalid username or password",
-          });
-      return res.json({
-        token: jwt.sign(
-          { email: user.email, fullName: user.fullName, _id: user._id },
-          "RESTFULAPIs"
-        ),
-      });
-    }
-  );
+
+  const user = await User.findOne({ username: username });
+  if (!user) return res.status(400).json({ message: "User not found" });
+
+  const validPassword = await bcrypt.compare(password, user.password);
+  if (!validPassword)
+    return res.status(400).json({ message: "Incorrect Password" });
+  try {
+    const { error } = await loginSchema.validateAsync(password);
+    if (error) return res.json({ message: error.details[0].message });
+    const token = jwt.sign({ _id: user._id }, process.env.TOKEN_SECRET);
+    res.header("auth-token", token).send(token);
+  } catch (err) {
+    res.json({ message: err.message });
+  }
 };
+
+
 
 module.exports = {
   signup,
