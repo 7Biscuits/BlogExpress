3b6358d72e5d76b91dbdd5c3deb0920899a04246
Rudransh Srivastava <rudranshsrivastava2525@gmail.com>
2023-06-02 02:47:39 +0530

    feat: signin integration done

--- src/pages/Signin.jsx
+++ src/pages/Signin.jsx
@@ -2,8 +2,18 @@ import Form from "../components/Form"
 
 const Signin = () => {
 
+  const signin = async (username, password) => {
+    await fetch('http://localhost:8080/auth/signin', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ username, password })
+    }).then(async response => alert(await response.text()))
+  }
+
   return (
-    <Form heading={"Sign in"} label1={"Username"} label2={"Password"} footerA1={"Forgot password?"} footerText={"Don't have an account?"} footerA2={"Sign up"} redirectLink={"/signup"} />
+    <Form heading={"Sign in"} label1={"Username"} label2={"Password"} footerA1={"Forgot password?"} footerText={"Don't have an account?"} footerA2={"Sign up"} redirectLink={"/signup"} userInfo={(username, password) => signin(username, password)} />
   )
 }
 
