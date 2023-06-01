052c9cac91c588e00ede7fd72ae2b954a329df93
Rudransh Srivastava <rudranshsrivastava2525@gmail.com>
2023-06-02 02:31:40 +0530

    feat: signup integration done

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,8 +1,19 @@
 import Form from "../components/Form"
 
 const Signup = () => {
+
+  const signup = async (username, password) => {
+    await fetch('http://localhost:8080/auth/signup', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ username, password })
+    }).then(async response => alert(await response.text()));
+  }
+
   return (
-    <Form heading={"Sign up"} label1={"Username"} label2={"Password"} footerA1={""} footerText={"Already have an account?"} footerA2={"Login"} redirectLink={"/"} />
+    <Form heading={"Sign up"} label1={"Username"} label2={"Password"} footerA1={""} footerText={"Already have an account?"} footerA2={"Login"} redirectLink={"/"} userInfo={(username, password) => signup(username, password)} />
   )
 }
 
