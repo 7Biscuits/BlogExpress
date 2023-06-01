aaab22bc993ce0c86b9fdaf352536bf71711cd49
Rudransh Srivastava <rudranshsrivastava@Rudranshs-MacBook-Air.local>
2023-06-01 18:44:55 +0530

    feat: removed unused controllers

--- src/api/routes/userRoutes.js
+++ src/api/routes/userRoutes.js
@@ -2,14 +2,11 @@ const express = require("express");
 const {
   signup,
   signin,
-  signinRequired,
-  profile,
 } = require("../controllers/userController");
 
 const router = express.Router();
 
 router.route('/auth/signup').post(signup);
 router.route('/auth/signin').post(signin);
-router.route('/tasks').post(signinRequired, profile);
 
 module.exports = router;
