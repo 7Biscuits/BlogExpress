448f91b8f76a6ab375adf2aa0de70ce1c17d7311
Rudransh Srivastava <rudranshsrivastava@Rudranshs-MacBook-Air.local>
2023-06-01 21:22:07 +0530

    feat: added authentication route

--- src/api/app.js
+++ src/api/app.js
@@ -22,29 +22,10 @@ mongoose.connect(process.env.MONGODB_URI, {
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.use((req, res, next) => {
-  if (
-    req.headers &&
-    req.headers.authorization &&
-    req.headers.authorization.split(" ")[0] === "JWT"
-  ) {
-    jwt.verify(
-      req.headers.authorization.split(" ")[1],
-      "RESTFULAPIs",
-      (err, decode) => {
-        if (err) req.user = undefined;
-        req.user = decode;
-        next();
-      }
-    );
-  } else {
-    req.user = undefined;
-    next();
-  }
-});
-
+const authRoutes = require("./routes/authRoutes");
 const userRoutes = require("./routes/userRoutes");
 
+app.use("/auth", authRoutes);
 app.use("/users", userRoutes);
 
 app.listen(8080, () => {
