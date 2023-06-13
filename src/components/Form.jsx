f524f8dbc6a93eec130f27b32148aaede9a84873
Rudransh Srivastava <rudranshsrivastava2525@gmail.com>
2023-06-13 16:47:20 +0530

    feat: changed the form UI

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,71 +1,48 @@
 import { Link } from "react-router-dom"
+import Button from "./Button"
 
-const Form = ({ heading, label1, label2, footerText, footerA1, footerA2, redirectLink, userInfo }) => {
+const Form = ({ heading, text, text2, text3, btnText, link, fplink, userInfo }) => {
 
     const handleSubmit = (e) => {
-        e.preventDefault()
+        e.preventDefault();
 
-        if (!e.target.username.value) return alert('Please enter your username')
-        if (!e.target.password.value) return alert('Please enter your password')
+        if (!e.target.username.value) return alert('Please enter your username');
+        if (!e.target.password.value) return alert('Please enter your password');
 
         userInfo(e.target.username.value, e.target.password.value)
+
+        e.target.username.value = ""
+        e.target.password.value = ""
     }
 
     return (
-        <div className="relative flex flex-col justify-center min-h-screen overflow-hidden">
-            <div className="w-full p-6 m-auto bg-gray-900 rounded-md shadow-md max-w-md lg:max-w-xl md:max-w-xl xl:max-w-xl">
-                <h1 className="text-3xl font-semibold text-center text-blue-600">
-                    {heading}
-                </h1>
-                <form onSubmit={handleSubmit} className="mt-6">
-                    <div className="mb-2">
-                        <label
-                            for="username"
-                            className="block text-sm font-semibold text-blue-600"
-                        >
-                            {label1}
-                        </label>
-                        <input
-                            type="username"
-                            name="username"
-                            className="block w-full px-4 py-2 mt-2 text-blue-600 bg-white border rounded-md focus:border-blue-400 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40"
-                        />
+        <div class="relative flex flex-col justify-center min-h-screen max-w-sm mx-auto overflow-hidden bg-white rounded-lg shadow-md dark:bg-gray-800">
+            <div class="px-6 py-4">
+                <h3 class="mt-3 text-xl font-medium text-center text-gray-600 dark:text-gray-200">{heading}</h3>
+
+                <p class="mt-1 text-center text-gray-200 dark:text-gray-400">{text}</p>
+
+                <form onSubmit={handleSubmit}>
+                    <div class="w-full mt-4">
+                        <input class="block w-full px-4 py-2 mt-2 text-gray-200 placeholder-gray-500 bg-white border rounded-lg dark:bg-gray-800 dark:border-gray-600 dark:placeholder-gray-400 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-opacity-40 focus:outline-none focus:ring focus:ring-blue-300" type="username" name="username" placeholder="Username" aria-label="Username" />
                     </div>
-                    <div className="mb-2">
-                        <label
-                            for="password"
-                            className="block text-sm font-semibold text-blue-600"
-                        >
-                            {label2}
-                        </label>
-                        <input
-                            type="password"
-                            name="password"
-                            className="block w-full px-4 py-2 mt-2 text-blue-600 bg-white border rounded-md focus:border-blue-400 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40"
-                        />
+
+                    <div class="w-full mt-4">
+                        <input class="block w-full px-4 py-2 mt-2 text-gray-200 placeholder-gray-500 bg-white border rounded-lg dark:bg-gray-800 dark:border-gray-600 dark:placeholder-gray-400 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-opacity-40 focus:outline-none focus:ring focus:ring-blue-300" type="password" name="password" placeholder="Password" aria-label="Password" />
                     </div>
-                    <a
-                        href="#"
-                        className="text-xs text-blue-600 hover:underline"
-                    >
-                        {footerA1}
-                    </a>
-                    <div className="mt-6">
-                        <button className="w-full px-4 py-2 tracking-wide text-white transition-colors duration-200 transform bg-blue-600 rounded-md hover:bg-blue-700 focus:outline-none focus:bg-blue-700">
-                            {heading}
-                        </button>
+
+                    <div class="flex items-center justify-between mt-4">
+                        <Link to="#" class="text-sm text-gray-600 dark:text-gray-200 hover:text-gray-400">{fplink}</Link>
+
+                        <Button text={btnText} />
                     </div>
                 </form>
+            </div>
 
-                <p className="mt-8 text-xs font-light text-center text-gray-500">
-                    {" "}
-                    {footerText}{" "}
-                    <Link to={redirectLink}
-                        className="font-medium text-blue-600 hover:underline"
-                    >
-                        {footerA2}
-                    </Link>
-                </p>
+            <div class="flex items-center justify-center py-4 text-center bg-gray-50 dark:bg-gray-700">
+                <span class="text-sm text-gray-600 dark:text-gray-200">{text2}</span>
+
+                <Link to={link} class="mx-2 text-sm font-bold text-blue-500 dark:text-blue-400 hover:underline">{text3}</Link>
             </div>
         </div>
     )
