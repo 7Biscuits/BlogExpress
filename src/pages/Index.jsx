f0859c2872f1d2f74fa0861c7315e5daa1e1fd62
Rudransh Srivastava <rudranshsrivastava2525@gmail.com>
2023-06-14 02:23:45 +0530

    feat: added components

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -1,7 +1,15 @@
+import Hero from "../components/Hero"
+import RecentBlogs from "../components/RecentBlogs"
+
 const Index = () => {
 
   return (
-    <></>
+    <>
+      <div className="relative min-h-screen top-52 flex flex-col justify-center">
+        <Hero />
+        <RecentBlogs />
+      </div>
+    </>
   )
 }
 
