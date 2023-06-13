923eabbf1e81c596d1439f979aaec3636c314759
Rudransh Srivastava <rudranshsrivastava2525@gmail.com>
2023-06-13 16:47:49 +0530

    refactor

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import { BrowserRouter, Routes, Route, redirect } from "react-router-dom";
-import { useState, useEffect } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useState } from "react";
+import Index from "./pages/Index";
 import Navbar from "./components/Navbar";
 import Signin from "./pages/Signin";
 import Signup from "./pages/Signup";
@@ -8,26 +9,41 @@ import Blogs from "./pages/Blogs";
 import CreatePost from "./pages/CreatePost";
 import NoPage from "./pages/NoPage";
 
-export default function App() {
-
-  let [title, setTitle] = useState('')
-  let [content, setContent] = useState('')
+const App = () => {
+  let [title, setTitle] = useState("");
+  let [content, setContent] = useState("");
 
   return (
-    <div className="max-w-full min-h-screen dark:bg-gray-800">
-      <Navbar />
-      <BrowserRouter>
+    <BrowserRouter>
+      <div className="max-w-full min-h-screen dark:bg-gray-800">
+        <Navbar />
         <Routes>
           <Route path="/">
-            <Route path="" element={<Signin />} />
-            <Route path="/signup" element={<Signup />} />
-            <Route path="blog" element={<Blog title={title} content={content} />} />
+            <Route index element={<Index />} />
+            <Route path="signin" element={<Signin />} />
+            <Route path="signup" element={<Signup />} />
+            <Route
+              path="blog"
+              element={<Blog title={title} content={content} />}
+            />
             <Route path="posts" element={<Blogs />} />
-            <Route path="create" element={<CreatePost newBlog={(title, content) => { setTitle(title); setContent(content); }} />} />
+            <Route
+              path="create"
+              element={
+                <CreatePost
+                  newBlog={(title, content) => {
+                    setTitle(title);
+                    setContent(content);
+                  }}
+                />
+              }
+            />
             <Route path="*" element={<NoPage />} />
           </Route>
         </Routes>
-      </BrowserRouter>
-    </div>
+      </div>
+    </BrowserRouter>
   );
 }
+
+export default App;
