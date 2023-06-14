d402423b07e835a1026008d3313f595c0ec18f28
Rudransh Srivastava <rudranshsrivastava2525@gmail.com>
2023-06-14 18:52:36 +0530

    fix: remove href in Link tag

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -9,7 +9,7 @@ const Hero = () => {
                 <TypingAnimation string1={"Create and Customize Your Blog"} string2={"Easy Content Creation"} string3={"Engage with Readers"} string4={"Discover and Follow Other Bloggers"} string5={"User-Friendly & Responsive Interface"} />
             </div>
             <div className="flex flex-row gap-5 my-2">
-                <Link to={"/signup"} href="/signup" className="px-4 py-2 md:px-5 md:py-3 text-sm md:text-md text-white bg-purple-600 hover:bg-purple-800 rounded-md cursor-pointer ease-in-out duration-100">Sign up</Link>
+                <Link to={"/signup"} className="px-4 py-2 md:px-5 md:py-3 text-sm md:text-md text-white bg-purple-600 hover:bg-purple-800 rounded-md cursor-pointer ease-in-out duration-100">Sign up</Link>
                 <a className="px-4 py-2 md:px-5 md:py-3 text-sm md:text-md text-purple-600 bg-white hover:bg-slate-200 rounded-md cursor-pointer ease-in-out duration-100">Discover</a>
             </div>
         </div>
